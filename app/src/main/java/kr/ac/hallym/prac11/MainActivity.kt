2f94c99a7c146fa7f0fa0fe9720c21f416b25fc0
Lee SeungHyun <leesh0545@gmail.com>
2022-12-15 00:51:12 +0900

    MB_Prac 11

---
+++ app/src/main/java/kr/ac/hallym/prac11/MainActivity.kt
@@ -0,0 +1,71 @@
+package kr.ac.hallym.prac11
+
+import android.app.Instrumentation.ActivityResult
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import kr.ac.hallym.prac11.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var requestPermissionLauncher: ActivityResultLauncher<Array<String>>
+    lateinit var requestContactsLauncher: ActivityResultLauncher<Intent>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        requestPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()){
+            for(entry in it.entries){
+                if(entry.key == "android.permission.READ_CONTACTS" && entry.value){
+                    Log.d("kkang","granted ok...")
+                    val intent = Intent(Intent.ACTION_PICK, ContactsContract.CommonDataKinds.Phone.CONTENT_URI)
+                    requestContactsLauncher.launch(intent)
+                }
+                else{
+                    Toast.makeText(this, "required permission...",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        requestContactsLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()){
+            if(it.resultCode == RESULT_OK){
+                Log.d("kkang","${it.data?.data}")
+                val cursor = contentResolver.query(it!!.data!!.data!!, arrayOf<String>(
+                    ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                    ContactsContract.CommonDataKinds.Phone.NUMBER
+                ),null,null,null)
+
+                Log.d("kkang","cursor size: ${cursor?.count}")
+                if(cursor!!.moveToFirst()){
+                    val name = cursor?.getString(0)
+                    val phone = cursor?.getString(1)
+                    binding.resultContact.text = "name: $name, phone: $phone"
+                }
+            }
+        }
+
+        binding.contactButton.setOnClickListener {
+            if(ContextCompat.checkSelfPermission(this, "android.permission.READ_CONTACTS")
+            == PackageManager.PERMISSION_GRANTED){
+                val intent = Intent(Intent.ACTION_PICK,ContactsContract.CommonDataKinds.Phone.CONTENT_URI)
+                requestContactsLauncher.launch(intent)
+            }
+            else{
+                requestPermissionLauncher.launch(arrayOf("android.permission.READ_CONTACTS"))
+            }
+        }
+
+    }
+}
